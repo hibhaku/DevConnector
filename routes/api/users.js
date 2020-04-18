875a6ed4554ada71b05719a3ad85da68215ff8c7
iamhbkulkarni <iamhbkulkarni@gmail.com>
2020-04-18 20:57:10 +0530

    Modify api/users POST route
    Add jwt code

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -57,8 +59,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      // Return json-web-token
-      res.send('User registered');
+      // Send back jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
